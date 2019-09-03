77f5b8e609dc4fa8f4c6c61074b1f3af68977a74
fabsgc <fabienbeaudimi@hotmail.fr>
2019-09-03 23:06:00 +0200

    Working on Linuw window system

--- Source/Framework/Core/Private/Linux/TeLinuxWindow.cpp
+++ Source/Framework/Core/Private/Linux/TeLinuxWindow.cpp
@@ -5,15 +5,81 @@ namespace te
 {
     struct LinuxWindow::Pimpl
 	{
+		// TODO
     };
 
     LinuxWindow::LinuxWindow(const WINDOW_DESC &desc)
 	{
-		_data = te_new<Pimpl>();
+		_data = te_new<Pimpl>(); // TODO
     }
 
     LinuxWindow::~LinuxWindow()
 	{
-		te_delete(_data);
+		te_delete(_data); // TODO
+	}
+
+	void LinuxWindow::Move(INT32 x, INT32 y)
+	{
+		// TODO
+	}
+
+	void LinuxWindow::Resize(UINT32 width, UINT32 height)
+	{
+		// TODO
+	}
+
+	void LinuxWindow::Hide()
+	{
+		// TODO
+	}
+
+	void LinuxWindow::Show()
+	{
+		// TODO
+	}
+
+	void LinuxWindow::Maximize()
+	{
+		// TODO
+	}
+
+	void LinuxWindow::Minimize()
+	{
+		// TODO
+	}
+
+	void LinuxWindow::Restore()
+	{
+		// TODO
+	}
+
+	INT32 LinuxWindow::GetLeft() const
+	{
+		return 0; // TODO
+	}
+
+	INT32 LinuxWindow::GetTop() const
+	{
+		return 0; // TODO
+	}
+
+	UINT32 LinuxWindow::GetWidth() const
+	{
+		return 0; // TODO
+	}
+
+	UINT32 LinuxWindow::GetHeight() const
+	{
+		return 0; // TODO
+	}
+
+	Vector2I LinuxWindow::WindowToScreenPos(const Vector2I& windowPos) const
+	{
+		return Vector2I(); // TODO
+	}
+
+	Vector2I LinuxWindow::ScreenToWindowPos(const Vector2I& screenPos) const
+	{
+		return Vector2I(); // TODO
 	}
 }
--- Source/Framework/Core/Private/Linux/TeLinuxWindow.h
+++ Source/Framework/Core/Private/Linux/TeLinuxWindow.h
@@ -2,23 +2,74 @@
 
 #include "Prerequisites/TePrerequisitesUtility.h"
 #include "Math/TeVector2I.h"
+#include <X11/X.h>
+#include <X11/Xutil.h>
 
 namespace te
 {
     /**	Descriptor used for creating a platform specific native window. */
 	struct WINDOW_DESC
 	{
+		INT32 X, Y;
+		UINT32 Width, Height;
+		UINT32 Screen;
+		String Title;
+		bool ShowDecorations;
+		bool AllowResize;
+		bool Modal;
+		bool ShowOnTaskBar;
+		bool Hidden;
+		::Window Parent;
+		::Window External;
+		XVisualInfo VisualInfo;
+		// SPtr<PixelData> background; TODO
     };
 
-    /**
-	 * Represents a X11 window. Note that all accesses (including creation and destruction) to objects of this class must
-	 * be locked by the main X11 lock accessible through LinuxPlatform.
-	 */
+    /** Represents a X11 window. */
 	class TE_UTILITY_EXPORT LinuxWindow
 	{
 	public:
 		LinuxWindow(const WINDOW_DESC& desc);
 		~LinuxWindow();
+
+		/**	Returns position of the left-most border of the window, relative to the screen. */
+        INT32 GetLeft() const;
+
+        /**	Returns position of the top-most border of the window, relative to the screen. */
+        INT32 GetTop() const;
+
+        /**	Returns width of the window in pixels. */
+        UINT32 GetWidth() const;
+
+        /**	Returns height of the window in pixels. */
+        UINT32 GetHeight() const;
+
+		/** Hides the window. */
+		void Hide();
+
+		/** Shows (unhides) the window. */
+		void Show();
+
+		/**	Minimizes the window. */
+		void Minimize();
+
+		/**	Maximizes the window over the entire current screen. */
+		void Maximize();
+
+		/**	Restores the window to original position and size if it is minimized or maximized. */
+		void Restore();
+
+		/**	Change the size of the window. */
+		void Resize(UINT32 width, UINT32 height);
+
+		/**	Reposition the window. */
+		void Move(INT32 left, INT32 top);
+
+		/**	Converts screen position into window local position. */
+		Vector2I ScreenToWindowPos(const Vector2I& screenPos) const;
+
+		/**	Converts window local position to screen position. */
+		Vector2I WindowToScreenPos(const Vector2I& windowPos) const;
 
     private:
         struct Pimpl;
