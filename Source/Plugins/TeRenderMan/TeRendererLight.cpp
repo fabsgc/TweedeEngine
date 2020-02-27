02223c7a35f197e8eef1d2f995c7538030ba121e
fabsgc <fabienbeaudimi@hotmail.fr>
2020-02-27 23:13:02 +0100

    WIP working on light rendering

--- Source/Plugins/TeRenderMan/TeRendererLight.cpp
+++ Source/Plugins/TeRenderMan/TeRendererLight.cpp
@@ -1,4 +1,6 @@
 #include "TeRendererLight.h"
+#include "TeRendererView.h"
+#include "TeRendererScene.h"
 
 namespace te
 {
@@ -40,7 +42,81 @@ namespace te
 
     void VisibleLightData::Update(const SceneInfo& sceneInfo, const RendererViewGroup& viewGroup)
     {
-        // TODO
+        const VisibilityInfo& visibility = viewGroup.GetVisibilityInfo();
+
+        for (UINT32 i = 0; i < (UINT32)LightType::Count; i++)
+            _visibleLights[i].clear();
+
+        // Generate a list of lights and their GPU buffers
+        UINT32 numDirLights = (UINT32)sceneInfo.DirectionalLights.size();
+        for (UINT32 i = 0; i < numDirLights; i++)
+            _visibleLights[(UINT32)LightType::Directional].push_back(&sceneInfo.DirectionalLights[i]);
+
+        UINT32 numRadialLights = (UINT32)sceneInfo.RadialLights.size();
+        for (UINT32 i = 0; i < numRadialLights; i++)
+        {
+            if (!visibility.RadialLights[i])
+                continue;
+
+            _visibleLights[(UINT32)LightType::Radial].push_back(&sceneInfo.RadialLights[i]);
+        }
+
+        UINT32 numSpotLights = (UINT32)sceneInfo.SpotLights.size();
+        for (UINT32 i = 0; i < numSpotLights; i++)
+        {
+            if (!visibility.SpotLights[i])
+                continue;
+
+            _visibleLights[(UINT32)LightType::Spot].push_back(&sceneInfo.SpotLights[i]);
+        }
+
+        for (UINT32 i = 0; i < (UINT32)LightType::Count; i++)
+            _numLights[i] = (UINT32)_visibleLights[i].size();
+
+        // Partition all visible lights so that unshadowed ones come first
+        auto partition = [](Vector<const RendererLight*>& entries)
+        {
+            UINT32 numUnshadowed = 0;
+            int first = -1;
+            for (UINT32 i = 0; i < (UINT32)entries.size(); ++i)
+            {
+                if (entries[i]->_internal->GetCastsShadow())
+                {
+                    first = i;
+                    break;
+                }
+                else
+                    ++numUnshadowed;
+            }
+
+            if (first != -1)
+            {
+                for (UINT32 i = first + 1; i < (UINT32)entries.size(); ++i)
+                {
+                    if (!entries[i]->_internal->GetCastsShadow())
+                    {
+                        std::swap(entries[i], entries[first]);
+                        ++numUnshadowed;
+                    }
+                }
+            }
+
+            return numUnshadowed;
+        };
+
+        for (UINT32 i = 0; i < (UINT32)LightType::Count; i++)
+            _numShadowedLights[i] = _numLights[i] - partition(_visibleLights[i]);
+
+        // Generate light data to initialize the GPU buffer with
+        _visibleLightData.clear();
+        for (auto& lightsPerType : _visibleLights)
+        {
+            for (auto& entry : lightsPerType)
+            {
+                _visibleLightData.push_back(LightData());
+                entry->GetParameters(_visibleLightData.back());
+            }
+        }
     }
 
     void VisibleLightData::GatherInfluencingLights(const Bounds& bounds,
