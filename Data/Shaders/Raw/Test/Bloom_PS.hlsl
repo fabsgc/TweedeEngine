8fa9383222518e867acd9e2108f8e1dfda95acfe
fabsgc <fabienbeaudimi@hotmail.fr>
2020-03-01 15:09:18 +0100

    WIP bloom

---
+++ Data/Shaders/Raw/Test/Bloom_PS.hlsl
@@ -0,0 +1,30 @@
+#include "Include/PostProcessBase.hlsli"
+
+cbuffer PerFrameBuffer : register(b0)
+{
+    uint gMSAACount;
+}
+
+SamplerState BilinearSampler : register(s0);
+
+Texture2D SourceMap : register(t0);
+Texture2DMS<float4> SourceMapMS : register(t1);
+
+float4 main( PS_INPUT IN ) : SV_Target0
+{
+    float4 color = (float4)0;
+
+    if(gMSAACount > 1)
+    {
+        float4 sum = float4(0, 0, 0, 0);
+
+        for(uint i = 0; i < gMSAACount; i++)
+            sum += SourceMapMS.Load(IN.Texture, i);
+
+        color = sum / gMSAACount;
+    }
+
+    color = SourceMap.Sample(BilinearSampler, IN.Texture);
+
+    return color;
+}
---
+++ Data/Shaders/Raw/Test/Bloom_VS.hlsl
@@ -0,0 +1,12 @@
+#include "Include/PostProcessBase.hlsli"
+
+VS_OUTPUT main( VS_INPUT IN )
+{
+    VS_OUTPUT OUT = (VS_OUTPUT)0;
+
+    OUT.Position = float4(IN.ScreenPosition, 1);
+    OUT.Texture = IN.Texture;
+    OUT.ScreenPosition = IN.ScreenPosition.xy;
+
+    return OUT;
+}
