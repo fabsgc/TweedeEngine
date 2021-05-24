f0d6501e424b8d4b3254cb6603ee26588b02a52d
fabsgc <fabienbeaudimi@hotmail.fr>
2021-05-25 00:23:06 +0200

    [TePhysics] Working on HingeJoint

--- Source/Framework/Core/Components/TeCHingeJoint.cpp
+++ Source/Framework/Core/Components/TeCHingeJoint.cpp
@@ -29,4 +29,109 @@ namespace te
     {
         CJoint::Clone(static_object_cast<CJoint>(c));
     }
+
+    void CHingeJoint::SetLimitSoftness(float softness)
+    {
+        if (_limitSoftness == softness)
+            return;
+
+        _limitSoftness = Math::Clamp(softness, 0.0f, 1.0f);
+
+        if (_internal != nullptr)
+            std::static_pointer_cast<HingeJoint>(_internal)->SetLimitSoftness(softness);
+    }
+
+    void CHingeJoint::SetLimitBias(float bias)
+    {
+        if (_limitBias == bias)
+            return;
+
+        _limitBias = Math::Clamp(bias, 0.0f, 1.0f);
+
+        if (_internal != nullptr)
+            std::static_pointer_cast<HingeJoint>(_internal)->SetLimitBias(bias);
+    }
+
+    void CHingeJoint::SetLimitRelaxation(float relaxation)
+    {
+        if (_limitRelaxation == relaxation)
+            return;
+
+        _limitRelaxation = Math::Clamp(relaxation, 0.0f, 1.0f);
+
+        if (_internal != nullptr)
+            std::static_pointer_cast<HingeJoint>(_internal)->SetLimitRelaxation(relaxation);
+    }
+
+    void CHingeJoint::SetLimitHigh(Degree limitHigh)
+    {
+        if (_limitHigh == limitHigh)
+            return;
+
+        _limitHigh = limitHigh;
+
+        if (_limitHigh < _limitLow)
+            _limitLow = _limitLow;
+
+        if (_internal != nullptr)
+            std::static_pointer_cast<HingeJoint>(_internal)->SetLimitHigh(_limitHigh);
+    }
+
+    void CHingeJoint::SetLimitLow(Degree limitLow)
+    {
+        if (_limitLow == limitLow)
+            return;
+
+        _limitLow = limitLow;
+
+        if (_limitLow > _limitHigh)
+            _limitLow = _limitLow;
+
+        if (_internal != nullptr)
+            std::static_pointer_cast<HingeJoint>(_internal)->SetLimitLow(_limitLow);
+    }
+
+    void CHingeJoint::SetAngularOnly(bool angularOnly)
+    {
+        if (_angularOnly == angularOnly)
+            return;
+
+        _angularOnly = angularOnly;
+
+        if (_internal != nullptr)
+            std::static_pointer_cast<HingeJoint>(_internal)->SetAngularOnly(angularOnly);
+    }
+
+    void CHingeJoint::SetMotorEnabled(bool motorEnabled)
+    {
+        if (_motorEnabled == motorEnabled)
+            return;
+
+        _motorEnabled = motorEnabled;
+
+        if (_internal != nullptr)
+            std::static_pointer_cast<HingeJoint>(_internal)->SetMotorEnabled(motorEnabled);
+    }
+
+    void CHingeJoint::SetMaxMotorImpulse(float motorImpulse)
+    {
+        if (_motorImpulse == motorImpulse)
+            return;
+
+        _motorImpulse = motorImpulse;
+
+        if (_internal != nullptr)
+            std::static_pointer_cast<HingeJoint>(_internal)->SetMaxMotorImpulse(motorImpulse);
+    }
+
+    void CHingeJoint::SetMotorVelocity(float motorVelocity)
+    {
+        if (_motorVelocity == motorVelocity)
+            return;
+
+        _motorVelocity = motorVelocity;
+
+        if (_internal != nullptr)
+            std::static_pointer_cast<HingeJoint>(_internal)->SetMotorVelocity(motorVelocity);
+    }
 }
--- Source/Framework/Core/Components/TeCHingeJoint.h
+++ Source/Framework/Core/Components/TeCHingeJoint.h
@@ -22,6 +22,60 @@ namespace te
         /** @copydoc Component::Clone */
         void Clone(const HHingeJoint& c);
 
+        /** @copydoc HingeJoint::SetLimitSoftness */
+        virtual void SetLimitSoftness(float softness);
+
+        /** @copydoc HingeJoint::GetLimitSoftness */
+        const float GetLimitSoftness() const { return _limitSoftness; }
+
+        /** @copydoc HingeJoint::SetLimitBias */
+        virtual void SetLimitBias(float bias);
+
+        /** @copydoc HingeJoint::GetLimitBias */
+        const float GetLimitBias() const { return _limitBias; }
+
+        /** @copydoc HingeJoint::SetLimitRelaxation */
+        virtual void SetLimitRelaxation(float relaxation);
+
+        /** @copydoc HingeJoint::GetLimitRelaxation */
+        const float GetLimitRelaxation() const { return _limitRelaxation; }
+
+        /** @copydoc HingeJoint::SetLimitHigh */
+        virtual void SetLimitHigh(Degree limitHigh);
+
+        /** @copydoc HingeJoint::GetLimitHigh */
+        const Degree GetLimitHigh() const { return _limitHigh; }
+
+        /** @copydoc HingeJoint::SetLimitLow */
+        virtual void SetLimitLow(Degree limitLow);
+
+        /** @copydoc HingeJoint::GetLimitLow */
+        const Degree GetLimitLow() const { return _limitLow; }
+
+        /** @copydoc HingeJoint::SetAngularOnly */
+        virtual void SetAngularOnly(bool angularOnly);
+
+        /** @copydoc HingeJoint::GetAngularOnly */
+        const bool GetAngularOnly() const { return _angularOnly; }
+
+        /** @copydoc HingeJoint::SetMotorEnabled */
+        virtual void SetMotorEnabled(bool motorEnabled);
+
+        /** @copydoc HingeJoint::GetMotorEnabled */
+        const bool GetMotorEnabled() const { return _motorEnabled; }
+
+        /** @copydoc HingeJoint::SetMaxMotorImpulse */
+        virtual void SetMaxMotorImpulse(float motorImpulse);
+
+        /** @copydoc HingeJoint::GetMaxMotorImpulse */
+        const bool GetMaxMotorImpulse() const { return _motorImpulse; }
+
+        /** @copydoc HingeJoint::SetMotorVelocity */
+        virtual void SetMotorVelocity(float motorVelocity);
+
+        /** @copydoc HingeJoint::GetMotorVelocity */
+        const bool GetMotorVelocity() const { return _motorVelocity; }
+
     protected:
         friend class SceneObject;
 
@@ -30,5 +84,16 @@ namespace te
 
     protected:
         CHingeJoint(); // Serialization only
+
+    protected:
+        float _limitSoftness = 1.0f;
+        float _limitBias = 0.3f;
+        float _limitRelaxation = 1.0f;
+        Degree _limitHigh;
+        Degree _limitLow;
+        bool _angularOnly = false;
+        bool _motorEnabled = false;
+        float _motorImpulse = 0.0f;
+        float _motorVelocity = 0.0f;
     };
 }
