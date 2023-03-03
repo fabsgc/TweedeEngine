85e83c05e226b0cfba19a5f033fcdcfdcbb62db7
fabsgc <fabienbeaudimi@hotmail.fr>
2023-03-03 20:30:34 +0100

    [WIP] SSAO

--- Data/Shaders/HLSL/SSAOBlur_PS.hlsl
+++ Data/Shaders/HLSL/SSAOBlur_PS.hlsl
@@ -22,10 +22,10 @@ float4 main( PS_INPUT IN ) : SV_Target0
 {
     const int NUM_SAMPLES = 2;
     float centerDepth = ConvertFromDeviceZ(gCamera, DepthMap.Sample(BilinearSampler, IN.Texture).r);
-		
+
     float weightedSum = 0.0f;
     float weightSum = 0.0f;
-    
+
     float centerAO = InputMap.Sample(BilinearSampler, IN.Texture).r;
     weightedSum += centerAO;
     weightSum += 1.0f;
@@ -35,27 +35,27 @@ float4 main( PS_INPUT IN ) : SV_Target0
     for(int i = 1; i < (NUM_SAMPLES + 1); ++i)
     {
         float2 sampleUV = gPixelSize * i + IN.Texture;
-    
+
         float sampleAO = InputMap.Sample(BilinearSampler, sampleUV).r;
         float sampleDepth = ConvertFromDeviceZ(gCamera, DepthMap.Sample(BilinearSampler, sampleUV).r);
-        
+
         float weight = saturate(1.0f - abs(sampleDepth - centerDepth) * gInvDepthThreshold);
         weightedSum += sampleAO * weight;
         weightSum += weight;
     }
-    
+
     [unroll]
     for(int j = -NUM_SAMPLES; j < 0; ++j)
     {
         float2 sampleUV = gPixelSize * j + IN.Texture;
-    
+
         float sampleAO = InputMap.Sample(BilinearSampler, sampleUV).r;
         float sampleDepth = ConvertFromDeviceZ(gCamera, DepthMap.Sample(BilinearSampler, sampleUV).r);
-        
+
         float weight = saturate(1.0f - abs(sampleDepth - centerDepth) * gInvDepthThreshold);
         weightedSum += sampleAO * weight;
         weightSum += weight;
     }
-            
+
     return weightedSum / weightSum;
 }
--- Data/Shaders/HLSL/SSAO_PS.hlsl
+++ Data/Shaders/HLSL/SSAO_PS.hlsl
@@ -39,7 +39,7 @@ float2 NdcToDepthUV(float2 ndc)
 float3 GetViewSpacePos(float2 ndc, float depth)
 {
     float2 clipSpace = ndc * -depth;
-    
+
     // Use the tan(FOV/2) & aspect to move from clip to view space (basically just scaling).
     // This is the equivalent of multiplying by mixedToView matrix that's used in most
     // depth -> world space calculations, but if we make some assumptions we can avoid the
@@ -48,7 +48,7 @@ float3 GetViewSpacePos(float2 ndc, float depth)
     // The only entries that effect the coordinate are 0,0 and 1,1 entries in the matrix
     // (if the matrix is symmetric, which we assume is true), which are just the cotangent
     // of the half of the two aspect ratios.
-    
+
     return float3(clipSpace * gTanHalfFOV, depth);
 }
 
@@ -64,30 +64,30 @@ float GetUpsampledAO(float2 uv, float depth, float3 normal)
     uvs[6] = uv + float2(-1,  1) * gDownsampledPixelSize;
     uvs[7] = uv + float2( 0,  1) * gDownsampledPixelSize;
     uvs[8] = uv + float2( 1,  1) * gDownsampledPixelSize;
-    
+
     float weightedSum = 0.00001f;
     float weightSum = 0.00001f;
-    
+
     [unroll]
     for(int i = 0; i < 9; ++i)
     {
         // Get AO from previous step (half-resolution buffer)
         float sampleAO = DownsampledAOMap.Sample(BilinearClampedSampler, uvs[i]).r;
-        
+
         // Get filtered normal/depth
         float4 sampleNormalAndDepth = SetupAOMap.Sample(BilinearClampedSampler, uvs[i]);
         float3 sampleNormal = sampleNormalAndDepth.xyz * 2.0f - 1.0f;
         float sampleDepth = sampleNormalAndDepth.w;
-        
+
         // Compute sample contribution depending on how close it is to current
         // depth and normal
         float weight = saturate(1.0f - abs(sampleDepth - depth) * 0.3f);
         weight *= saturate(dot(sampleNormal, normal));
-        
+
         weightedSum += sampleAO * weight;
         weightSum += weight;
     }
-    
+
     return weightedSum / weightSum;
 }
 
@@ -110,7 +110,7 @@ float4 main( PS_INPUT IN ) : SV_Target0
 
         SAMPLES[0] = float2( 0.250f,  0.000f);
         SAMPLES[1] = float2(-0.250f,  0.433f);
-		SAMPLES[2] = float2(-0.375f, -0.649f);
+        SAMPLES[2] = float2(-0.375f, -0.649f);
     }
     else
     {
@@ -132,7 +132,7 @@ float4 main( PS_INPUT IN ) : SV_Target0
     {
         sceneDepth = ConvertFromDeviceZ(gCamera, DepthMap.Sample(BilinearClampedSampler, IN.Texture).r);
         worldNormal = NormalsMap.Sample(BilinearClampedSampler, IN.Texture).xyz * 2.0f - 1.0f;
-    }   
+    }
     else // Input from AO setup pass
     {
         aoSetup = SetupAOMap.Sample(BilinearClampedSampler, IN.Texture);
@@ -155,20 +155,20 @@ float4 main( PS_INPUT IN ) : SV_Target0
 
     // Get random rotation
     float2 rotateDir = (float2)0;
-    
+
     if (gQuality == 0) rotateDir = float2(0, 1); // No random rotation
     else rotateDir = RandomMap.Sample(BilinearSampler, IN.Texture * gRandomTileScale).rg * 2 - 1;
 
     // Scale by screen space sample radius
     rotateDir *= sampleRadius;
-    
+
     // Construct rotation matrix
     float2 rotateDir90 = float2(-rotateDir.y, rotateDir.x); // Rotate 90 degrees
     float2x2 rotateTfrm = float2x2(
         rotateDir.x, rotateDir90.x,
         rotateDir.y, rotateDir90.y
     );
-                
+
     float invRange = 1.0f / gSampleRadius;
 
     // For every sample, find the highest horizon angle in the direction of the sample
@@ -177,17 +177,17 @@ float4 main( PS_INPUT IN ) : SV_Target0
     for(uint i = 0; i < SAMPLE_COUNT; ++i)
     {
         float2 sampleOffset = mul(rotateTfrm, SAMPLES[i]);
-			
+
         // Step along the direction of the sample offset, looking for the maximum angle in two directions
         // (positive dir of the sample offset, and negative). Steps are weighted so that those that are
         // further away from the origin contribute less.
         float3 stepAccum = 0;
-        
+
         [unroll]
         for(uint j = 1; j <= SAMPLE_STEPS; ++j)
         {
             float scale = j / (float)SAMPLE_STEPS;
-					
+
             float2 screenPosL = IN.ScreenPosition + sampleOffset * scale;
             float2 screenPosR = IN.ScreenPosition - sampleOffset * scale;
 
@@ -199,7 +199,7 @@ float4 main( PS_INPUT IN ) : SV_Target0
             {
                 depthL = DepthMap.Sample(BilinearClampedSampler, NdcToDepthUV(screenPosL)).r;
                 depthR = DepthMap.Sample(BilinearClampedSampler, NdcToDepthUV(screenPosR)).r;
-            
+
                 depthL = ConvertFromDeviceZ(gCamera, depthL);
                 depthR = ConvertFromDeviceZ(gCamera, depthR);
             }
@@ -211,84 +211,84 @@ float4 main( PS_INPUT IN ) : SV_Target0
 
             float3 viewPosL = GetViewSpacePos(screenPosL, depthL);
             float3 viewPosR = GetViewSpacePos(screenPosR, depthR);
-            
+
             float3 diffL = viewPosL - viewPos;
             float3 diffR = viewPosR - viewPos;
-            
+
             float angleL = saturate(dot(diffL, viewNormal) * rsqrt(dot(diffL, diffL)));
             float angleR = saturate(dot(diffR, viewNormal) * rsqrt(dot(diffR, diffR)));
-            
+
             // Avoid blending if depths are too different to avoid leaking
             float weight = saturate(1.0f - length(diffL) * invRange);
             weight *= saturate(1.0f - length(diffR) * invRange);
-            
+
             float2 angles = float2(angleL, angleR);
             stepAccum = lerp(stepAccum, float3(max(angles, stepAccum.xy), 1), weight);
         }
 
         // Negate since higher angle means more occlusion
         float2 weightedValue = 1.0f - stepAccum.xy;
-				
+
         // Square to reduce impact on areas with low AO, and increase impact on areas with high AO
         weightedValue *= weightedValue;
-				
+
         // Multiply by weight since we calculate the weighted average
         weightedValue *= stepAccum.z;
-				
+
         // Accumulate sum total and weight total
         accumulator += float2(weightedValue.x + weightedValue.y, 2.0f * stepAccum.z);
     }
 
     float output = 0;
-			
+
     // Divide by total weight to get the weighted average
     output = accumulator.x / accumulator.y;
 
     if (gUpSample)
     {
         float upsampledAO = GetUpsampledAO(IN.Texture, sceneDepth, worldNormal);
-    
+
         // Note: 0.6f just an arbitrary constant that looks good. Make this adjustable externally?
         output = lerp(output, upsampledAO, 0.6f);
     }
-    
+
     if (gFinalPass)
     {
         // Fade out far away AO
         // Reference: 1 - saturate((depth - fadeDistance) / fadeRange)
         output = lerp(output, 1.0f, saturate(-sceneDepth * gFadeMultiplyAdd.x + gFadeMultiplyAdd.y));
-    
+
         // Adjust power and intensity
         output = 1.0f - saturate((1.0f - pow(output, gPower)) * gIntensity);
     }
-    
+
     // On quality 0 we don't blur at all. At qualities higher than 1 we use a proper bilateral blur.
     if (gQuality == 1)
     {
         // Perform a 2x2 ad-hoc blur to hide the dither pattern
         // Note: Ideally the blur would be 4x4 since the pattern is 4x4
-    
+
         float4 myVal = float4(output, viewNormal);
         float4 dX = ddx(myVal);
         float4 dY = ddy(myVal);
-        
+
         uint2 mod = (uint2)(IN.Position.xy) % 2;
         float4 horzVal = myVal - dX * (mod.x * 2 - 1);
         float4 vertVal = myVal - dY * (mod.y * 2 - 1);
-        
+
         // Do weighted average depending on how similar the normals are
         float weightHorz = saturate(pow(saturate(dot(viewNormal, horzVal.yzw)), 4.0f));
         float weightVert = saturate(pow(saturate(dot(viewNormal, vertVal.yzw)), 4.0f));
-        
+
         float myWeight = 1.0f;
         float invWeight = 1.0f / (myWeight + weightHorz + weightVert);
-        
+
         myWeight *= invWeight;
         weightHorz *= invWeight;
         weightVert *= invWeight;
-        
+
         output = output * myWeight + horzVal.r * weightHorz + vertVal.r * weightVert;
     }
-    
+
     return output;
 }
