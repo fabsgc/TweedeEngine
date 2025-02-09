e8ebab7a292a94270e050c6143e1f19086dfebc1
Fabien Beaujean <fabienbeaudimi@hotmail.fr>
2025-02-09 11:18:02 +0100

    [Utility] Reduce log message size

--- Source/Framework/Utility/Error/TeDebug.h
+++ Source/Framework/Utility/Error/TeDebug.h
@@ -17,21 +17,17 @@
 #endif
 
 #   ifndef TE_DEBUG
-#   define TE_DEBUG(message)                                                                     \
-        {                                                                                        \
-            ::std::ofstream logFile(TE_DEBUG_FILE, ::std::ios_base::out | ::std::ios_base::app); \
-                                                                                                 \
-            logFile << "Log: " << __FILENAME__ << ":" << __LINE__ << ::std::endl;                \
-            logFile << "Inside " << __FUNCTION__ << ::std::endl;                                 \
-            logFile << "Date: " << __DATE__ << ::std::endl;                                      \
-            logFile << "Time: " << __TIME__ << ::std::endl;                                      \
-            logFile << "Message: " << message << ::std::endl;                                    \
-            logFile << "############################################: " << ::std::endl;          \
-                                                                                                 \
-            ::std::cout << "Log: " << __FILENAME__ << ":" << __LINE__ << ::std::endl;            \
-            ::std::cout << "Inside " << __FUNCTION__ << ::std::endl;                             \
-            ::std::cout << "Message: " << message << ::std::endl;                                \
-            ::std::cout << "############################################: " << ::std::endl;      \
+#   define TE_DEBUG(message)                                                                                                                                     \
+        {                                                                                                                                                        \
+            ::std::ofstream logFile(TE_DEBUG_FILE, ::std::ios_base::out | ::std::ios_base::app);                                                                 \
+                                                                                                                                                                 \
+            logFile << "[" << __FILENAME__ << ":" << __LINE__ << "][" << __FUNCTION__ << "]" << "[" << __DATE__ << "]" << "[" << __TIME__ << "]" << ::std::endl; \
+            logFile << message << ::std::endl;                                                                                                                   \
+            logFile << "############################################: " << ::std::endl;                                                                          \
+                                                                                                                                                                 \
+            ::std::cout << "[" << __FILENAME__ << ":" << __LINE__ << "][" << __FUNCTION__ << "]" << ::std::endl;                                                 \
+            ::std::cout << message << ::std::endl;                                                                                                               \
+            ::std::cout << "############################################: " << ::std::endl;                                                                      \
         }
 #   endif
 
