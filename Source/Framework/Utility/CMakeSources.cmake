edd957ab610ad4dc519b0041b25b5b547bb0ead2
Fabien Beaujean <fabienbeaudimi@hotmail.fr>
2025-06-01 13:50:29 +0200

    [Build] Fix Linux build issue

--- Source/Framework/Utility/CMakeSources.cmake
+++ Source/Framework/Utility/CMakeSources.cmake
@@ -35,7 +35,7 @@ set(TE_UTILITY_INC_THIRDPARTY
     "Utility/ThirdParty/RenderDoc/replay/renderdoc_replay.h"
     "Utility/ThirdParty/RenderDoc/replay/vk_pipestate.h"
     "Utility/ThirdParty/Slugify/slugify.hpp"
-    "Utility/ThirdParty/Dds/dds.h"
+    "Utility/ThirdParty/dds/dds.h"
 )
 set(TE_UTILITY_SRC_THIRDPARTY
     "Utility/ThirdParty/Md5/md5.cpp"
